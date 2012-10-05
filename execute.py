c0d4f753b0af6d5dcefe4c87e84d068d193776e7
Steve Holden <steve@holdenweb.com>
2012-10-04 22:22:49 -0700

    Cope with pathological 'you forgot the terminating blank line' errors.

--- execute.py
+++ execute.py
@@ -7,10 +7,22 @@
 import code
 import sys
 
+if sys.argv[1] == "-o": # O'Reilly output
+    o, c = "<INS>", "</INS>"
+    del sys.argv[1]
+else:
+    o = c = ""
+
+def tagged(p, l, o, c): # returns prompted command
+    if l:
+        return "%s %s%s%s" % (p, o, l, c)
+    else:
+        return p
+
 SIloop = None # Used in pathological EOF
 exprs = (line[:-1] for line in open(sys.argv[1]))
 for e in exprs:
-    print '>>> %s' % e
+    print tagged('>>>', e, o, c)
     cmd = code.compile_command(e)
     while cmd is None:
         try:
@@ -22,7 +34,7 @@ for e in exprs:
             print >> sys.stderr, "[Attempting terminal newline insertion]"
             n = ""
         e += "\n" + n
-        print '... %s' % n
+        print tagged('...', n, o, c)
         cmd = code.compile_command(e)
     r = eval(cmd)
     if r:
